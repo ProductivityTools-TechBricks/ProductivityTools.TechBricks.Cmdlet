854a6bb8dd540eb4563e8aa02a162edaf1f09d0c
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-05-18 07:47:42 +0200

    initial commit

---
+++ ProductivityTools.TechBricks.Cmdlet/Shotcuts/ShortcutsCmdlet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.TechBricks.Cmdlet.Shotcuts
+{
+    [Cmdlet(VerbsCommon.Get,"Shortcuts")]
+    internal class ShortcutsCmdlet :PSCmdlet.PSCmdletPT
+    {
+        protected override void ProcessRecord()
+        {
+            WriteOutput("HEllo");
+            base.ProcessRecord();
+        }
+    }
+}
